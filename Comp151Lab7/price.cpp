27ed39dd80c09f269a7eef4c5da7980bbbf8c6f1
Pedro Navarrete Hurtado <navhpedro@gmail.com>
2018-01-19 23:08:21 -0600

    Finished Lab 7, Exercise 3

--- Comp151Lab7/price.cpp
+++ Comp151Lab7/price.cpp
@@ -1,15 +1,15 @@
 // This program will read in prices and store them into a two-dimensional array
 // It will print those prices in a table form.
 
-// PLACE YOUR NAME HERE
+// Pedro J. Navarrete
 
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
 
-const  MAXROWS = 10;
-const  MAXCOLS = 10;
+const  int MAXROWS = 10;
+const  int MAXCOLS = 10;
 
 typedef float PriceType[MAXROWS][MAXCOLS];    // creates a new data type
 											  // of a 2D array of floats
@@ -22,11 +22,11 @@ int main()
 
 {
 	int rowsUsed;                               // holds the number of rows used 
-    int colsUsed;                               // holds the number of columns used
+	int colsUsed;                               // holds the number of columns used
 	PriceType priceTable;                       // a 2D array holding the prices
 
 
-	
+
 	getPrices(priceTable, rowsUsed, colsUsed);  // calls getPrices to fill the array
 	printPrices(priceTable, rowsUsed, colsUsed);// calls printPrices to display array
 
@@ -52,18 +52,21 @@ void   getPrices(PriceType table, int& numOfRows, int& numOfCols)
 
 {
 
-	cout << "Please input the number of rows from 1 to "<< MAXROWS << endl;
+	cout << "Please input the number of rows from 1 to " << MAXROWS << endl;
 	cin >> numOfRows;
 
-	cout << "Please input the number of columns from 1 to "<< MAXCOLS << endl;
+	cout << "Please input the number of columns from 1 to " << MAXCOLS << endl;
 	cin >> numOfCols;
 
 
-	for (int row = 0;  row < numOfRows;  row++)
-    {
-		for (int col = 0; col < numOfCols;  col++)
-
+	for (int row = 0; row < numOfRows; row++)
+	{
+		for (int col = 0; col < numOfCols; col++) {
 			// Fill in the code to read and store the next value in the array
+			cout << "\nPlease input price of an item with 2 decimal places\n";
+			cin >> table[row][col];
+
+		}
 	}
 }
 
@@ -86,10 +89,14 @@ void   printPrices(PriceType table, int numOfRows, int numOfCols)
 
 	cout << fixed << showpoint << setprecision(2);
 
-	for (int row = 0;  row < numOfRows; row++)
+	for (int row = 0; row < numOfRows; row++)
 	{
-		for (int col = 0;  col < numOfCols; col++)
-
+		for (int col = 0; col < numOfCols; col++) {
 			// Fill in the code to print the table
+			cout << setw(8) << table[row][col] << " ";
+		}
+
+		cout << endl;
+
 	}
 }
